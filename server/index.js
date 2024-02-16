c408ea3f98f544b08252860a5d24d727ad574055
Eugene <yousucker8@gmail.com>
2024-02-16 23:18:35 +0700

    v1

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const app = express();
+const route = require('./route')
+
+
+app.use(cors({ origin: "*"}));
+app.use(route);
+
+
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+app.listen(5000, () => {
+    console.log('server is running')
+} )
