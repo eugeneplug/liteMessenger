5ae9e2662bd4ff145481e657a0f7489703ffcf72
Eugene <yousucker8@gmail.com>
2024-02-17 02:19:09 +0700

    v1.1

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import App from "./components/App";
 
-// import "./styles/main.css";
+import "./styles/main.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
